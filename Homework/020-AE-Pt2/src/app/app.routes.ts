bd57366a8cd4d15f709d848948d7af667be6eab5
Shawn <iamshawnhartline@gmail.com>
2025-03-13 16:40:25 -0500

    3-13-25 4:40

---
+++ Homework/020-AE-Pt2/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { UserCardComponent } from './user-card/user-card.component';
+
+export const routes: Routes = [
+  {
+    path: "",
+    component: UserCardComponent
+  }
+];
+ 
